682fbcb868c19a33aca8ce20a3b2dffa316b1b6d
Ten Mutunhire <tmutunhire@gmail.com>
2015-08-24 10:08:43 +0200

    Initial Commit

---
+++ SimpleImageTests/SimpleImageTests.swift
@@ -0,0 +1,36 @@
+//
+//  SimpleImageTests.swift
+//  SimpleImageTests
+//
+//  Created by Tomahawk Africa Tindo on 8/24/15.
+//  Copyright (c) 2015 Tomahawk Africa Tindo. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SimpleImageTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
